89ff0f5e63dfe566123787f9101f386d3be0bbe9
skajd1 <woochul102@gmail.com>
2022-12-23 15:38:22 +0900

    added css style

--- pomelo/validator.py
+++ pomelo/validator.py
@@ -1,6 +1,10 @@
 import re
+
+
+# URL 유효성 검증
 def is_url_valid(url):
     REGEX_URL = '[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
     if not re.fullmatch(REGEX_URL, url):
         return False
     return True
+
